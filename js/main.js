c63833277c57b6fbe950ae182cfc97132a0e7885
p1ratrulezzz <git@p1ratrulezzz.me>
2020-01-27 15:54:20 +0300

    First version

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(function() {
+  loadJsonFile('/data/images.json', init);
+
+  /**
+   * Main entrypoint.
+   *
+   * In this place the data file is loaded already.
+   *
+   * @param data
+   */
+  function init(data) {
+    let imageWrapper = document.getElementById('image-wrapper');
+
+    function updateImage() {
+      let images = data.images;
+      let imageInfo = images[random(0, images.length - 1)];
+      if (!imageInfo) {
+        return;
+      }
+
+      let imgEl = document.createElement('img');
+      if (imageInfo.src != null) {
+        imgEl.setAttribute('src', imageInfo.src);
+      }
+      else if (imageInfo.data != null) {
+        // @todo: Add this functionality.
+      }
+      else {
+        return;
+      }
+
+      imageWrapper.innerHTML = "";
+      imageWrapper.appendChild(imgEl);
+    }
+
+    updateImage();
+  }
+
+  window.random = function(min, max) {
+    return Math.round(Math.random() * (max - min + 1)) + min;
+  }
+})();
