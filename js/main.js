237c3327fa02d9d49c33bd2f6e26ce6438bc0f7a
p1ratrulezzz <git@p1ratrulezzz.me>
2020-01-28 14:20:27 +0300

    Shuffle array

--- js/main.js
+++ js/main.js
@@ -11,10 +11,37 @@
    */
   function init(data) {
     let imageWrapper = document.getElementById('image-wrapper');
+    let images = data.images;
+
+    /**
+     *
+     * @param arra1
+     * @returns {*}
+     * @link https://www.w3resource.com/javascript-exercises/javascript-array-exercise-17.php
+     */
+    function shuffleArray(arra1) {
+      let ctr = arra1.length, temp, index;
+
+      // While there are elements in the array
+      while (ctr > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * ctr);
+        // Decrease ctr by 1
+        ctr--;
+        // And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+      }
+      return arra1;
+    }
 
     function updateImage() {
-      let images = data.images;
-      let imageInfo = images[random(0, images.length - 1)];
+      if (images.length === 0) {
+        images = shuffleArray(data.images);
+      }
+
+      let imageInfo = images.pop();
       if (!imageInfo) {
         return;
       }
